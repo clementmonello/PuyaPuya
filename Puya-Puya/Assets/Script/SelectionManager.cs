5898915a37383d4e127aae5cdddf5d7cd245c902
Wawax007 <alexis.borteyru@gmail.com>
2023-04-11 15:31:44 +0200

    Ajout: ScriptablePlayer

--- Puya-Puya/Assets/Script/SelectionManager.cs
+++ Puya-Puya/Assets/Script/SelectionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,62 +6,49 @@ using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
-    public Button[] characterButtons; // Tableau de boutons pour les personnages
-    public Image[] selectedCharacters; // Tableau d'images pour les personnages sélectionnés par les joueurs
-    public GameObject[] okBanners; // Tableau d'objets pour les bannières "OK"
+    public Button[] characterButtons;
+    public Image selectedCharacter;
+    public GameObject okBanner; 
+    public Sprite[] characterImages; 
 
-    private int[] playerSelections = new int[2] {-1, -1}; // Indices des personnages sélectionnés par les joueurs (-1 = pas encore sélectionné)
+    public CharacterData selectedCharacterData;
+    
+    private int playerSelection = -1; 
 
     private void Start()
     {
-        // Active tous les boutons de personnage
         foreach (Button button in characterButtons)
         {
             button.gameObject.SetActive(true);
         }
+        
+        selectedCharacter.gameObject.SetActive(false);
+        okBanner.SetActive(false);
+    }
 
-        // Désactive les bannières "OK" pour les deux joueurs
-        foreach (GameObject banner in okBanners)
+    public void SelectCharacter(int characterIndex)
+    {
+        if (playerSelection == characterIndex)
         {
-            banner.SetActive(false);
+            selectedCharacter.gameObject.SetActive(false);
+            playerSelection = -1;
+            okBanner.SetActive(false);
+        }
+        else
+        {
+            selectedCharacter.sprite = characterImages[characterIndex];
+            selectedCharacter.gameObject.SetActive(true);
+            playerSelection = characterIndex;
+
+            okBanner.SetActive(true);
         }
     }
 
-    // Fonction appelée lorsqu'un joueur sélectionne un personnage
-    public void SelectCharacter(int playerIndex, int characterIndex)
+    public void DeselectCharacter()
     {
-        // Vérifie si le personnage a déjà été sélectionné par un autre joueur
-        if (playerSelections[1 - playerIndex] == characterIndex)
-        {
-            // Désélectionne le personnage pour l'autre joueur
-            selectedCharacters[1 - playerIndex].gameObject.SetActive(false);
-            playerSelections[1 - playerIndex] = -1;
-            okBanners[1 - playerIndex].SetActive(false);
-        }
+        selectedCharacter.gameObject.SetActive(false);
+        playerSelection = -1;
 
-        // Sélectionne le personnage pour le joueur actuel
-        selectedCharacters[playerIndex].sprite = characterButtons[characterIndex].GetComponent<Image>().sprite;
-        selectedCharacters[playerIndex].gameObject.SetActive(true);
-        playerSelections[playerIndex] = characterIndex;
-
-        // Vérifie si les deux joueurs ont sélectionné un personnage
-        if (playerSelections[0] != -1 && playerSelections[1] != -1)
-        {
-            // Active les bannières "OK" pour les deux joueurs
-            okBanners[0].SetActive(true);
-            okBanners[1].SetActive(true);
-        }
-    }
-
-    // Fonction appelée lorsqu'un joueur annule sa sélection de personnage
-    public void DeselectCharacter(int playerIndex)
-    {
-        // Désélectionne le personnage pour le joueur actuel
-        selectedCharacters[playerIndex].gameObject.SetActive(false);
-        playerSelections[playerIndex] = -1;
-
-        // Désactive les bannières "OK" pour les deux joueurs
-        okBanners[0].SetActive(false);
-        okBanners[1].SetActive(false);
+        okBanner.SetActive(false);
     }
 }
