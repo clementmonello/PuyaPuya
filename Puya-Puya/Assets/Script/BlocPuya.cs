ac651945ac7b81b1bb598c44b09cfb696e14ff45
voltkrugers <80946089+voltkrugers@users.noreply.github.com>
2023-05-17 09:01:53 +0200

    Update Code Puya +Dépalcement +Gameplay
    à l'aide d'une vidéo tuto

---
+++ Puya-Puya/Assets/Script/BlocPuya.cs
@@ -0,0 +1,221 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocPuya : MonoBehaviour
+{
+    public GameObject[] unitArray = new GameObject[2];
+
+    public float fallSpeed = 1;
+    public float interval = 0;
+
+    private Vector3 left = Vector3.left;
+    private Vector3 right = Vector3.right;
+    private Vector3 down = Vector3.down;
+    private Vector3 up = Vector3.up;
+
+    private bool puyoUnitDropsFinished = false;
+
+    void Start()
+    {
+        unitArray[0] = Instantiate((GameObject)Resources.Load("PuyaUnit"), transform.position, Quaternion.identity);
+        unitArray[1] = Instantiate((GameObject)Resources.Load("PuyaUnit"), new Vector2(transform.position.x + 1, transform.position.y), Quaternion.identity);
+        unitArray[0].transform.parent = gameObject.transform;
+        unitArray[1].transform.parent = gameObject.transform;
+        UpdateGameBoard();
+    }
+
+    void Update()
+    {
+        AutoDrop();       
+    }
+
+    void AutoDrop()
+    {
+        if (interval > fallSpeed)
+        {
+            MoveDown();
+            interval = 0;
+        }
+        else
+        {
+            interval += Time.deltaTime;
+        }
+    }
+
+
+    public void MoveLeft()
+    {
+        if (ValidMove(left))
+        {
+            Move(left, transform);
+        }
+    }
+
+    public void MoveRight()
+    {
+        if (ValidMove(right))
+        {
+            Move(right, transform);
+        }
+    }
+
+    public void MoveDown()
+    {
+        if (ValidMove(down))
+        {
+            Move(down, transform);
+        }
+        else
+        {
+            DisableSelf();
+        }
+    }
+
+    public void RotateLeft()
+    {
+        Vector3 vect = GetClockwiseRotationVector();
+        if (ValidRotate(vect))
+        {
+            Move(vect, unitArray[1].transform);
+        }
+    }
+
+    public void RotateRight()
+    {
+        Vector3 vect = GetCounterClockwiseRotationVector();
+        if (ValidRotate(vect))
+        {
+            Move(vect, unitArray[1].transform);
+        }
+    }
+
+    void Move(Vector3 vector, Transform target)
+    {
+        ClearCurrentGameboardPosition();
+        target.position += vector;
+        UpdateGameBoard();
+    }
+
+    void ClearCurrentGameboardPosition()
+    {
+        foreach (Transform puyaUnit in transform)
+        {
+            Grid.Clear(puyaUnit.transform.position.x, puyaUnit.transform.position.y);
+        }
+    }
+
+    void UpdateGameBoard()
+    {
+        foreach (Transform puyaUnit in transform)
+        {
+            Grid.Add(puyaUnit.position.x, puyaUnit.position.y, puyaUnit);
+        }
+    }
+
+    Vector3 GetClockwiseRotationVector()
+    {
+        Vector3 puyaUnitPos = RoundVector(unitArray[1].transform.position);
+
+        if (Vector3.Distance(puyaUnitPos + left, transform.position) == 0)
+        {
+            return new Vector3(-1, -1);
+        }
+        else if (Vector3.Distance(puyaUnitPos + up, transform.position) == 0)
+        {
+            return new Vector3(-1, +1);
+        }
+        else if (Vector3.Distance(puyaUnitPos + right, transform.position) == 0)
+        {
+            return new Vector3(+1, +1);
+        }
+        else if (Vector3.Distance(puyaUnitPos + down, transform.position) == 0)
+        {
+            return new Vector3(+1, -1);
+        }
+
+        return new Vector3(0, 0);
+    }
+
+    Vector3 GetCounterClockwiseRotationVector()
+    {
+        Vector3 puyaUnitPos = RoundVector(unitArray[1].transform.position);
+
+        if (Vector3.Distance(puyaUnitPos + left, transform.position) == 0)
+        {
+            return new Vector3(-1, +1);
+        }
+        else if (Vector3.Distance(puyaUnitPos + up, transform.position) == 0)
+        {
+            return new Vector3(+1, +1);
+        }
+        else if (Vector3.Distance(puyaUnitPos + right, transform.position) == 0)
+        {
+            return new Vector3(+1, -1);
+        }
+        else if (Vector3.Distance(puyaUnitPos + down, transform.position) == 0)
+        {
+            return new Vector3(-1, -1);
+        }
+
+        return new Vector3(0, 0);
+    }
+
+    bool ActivelyFalling()
+    {
+        return unitArray[0].GetComponent<PuyaUnit>().activelyFalling ||
+            unitArray[1].GetComponent<PuyaUnit>().activelyFalling;
+    }
+
+  
+
+    bool ValidMove(Vector3 direction)
+    {
+        foreach (Transform puya in transform)
+        {
+            Vector3 newPosition = new Vector3(puya.position.x + direction.x, puya.position.y + direction.y, 0);
+
+            if (!Grid.FreeSpace(newPosition, transform))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool ValidRotate(Vector3 direction)
+    {
+        Vector3 puyaPos = unitArray[1].transform.position;
+        Vector3 newPosition = new Vector3(puyaPos.x + direction.x, puyaPos.y + direction.y);
+        return Grid.FreeSpace(newPosition, transform);
+    }
+
+    private void DropPuyaUnits()
+    {
+        foreach (Transform puyaUnit in transform)
+        {
+            StartCoroutine(puyaUnit.gameObject.GetComponent<PuyaUnit>().DropToFloor());
+        }
+    }
+
+
+    public Vector3 RoundVector(Vector3 vect)
+    {
+        return new Vector2(Mathf.Round(vect.x), Mathf.Round(vect.y));
+    }
+
+    void DisableSelf()
+    {
+        gameObject.GetComponent<PlayerController>().enabled = false;
+        DropPuyaUnits();
+        enabled = false;
+        StartCoroutine(SpawnNextBlock());
+    }
+
+    IEnumerator SpawnNextBlock()
+    {
+        yield return new WaitUntil(() => !ActivelyFalling());
+
+        GameObject.Find("PuyaSpawner").GetComponent<PuyaSpawner>().SpawnPuyo();
+    }
+}
--- Puya-Puya/Assets/Script/Grid.cs
+++ Puya-Puya/Assets/Script/Grid.cs
@@ -2,83 +2,204 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class Grid : MonoBehaviour
 {
-    int nbRow = 12;
-    int nbColumn = 6;
-    public List<List<Puya>> grid;
+    public static Transform[,] gameBoard = new Transform[6, 12];
 
-    // Start is called before the first frame update
-    void Start()
+    public static bool WithinBorders(Vector3 target)
     {
-        grid = new List<List<Puya>>();
-        for (int i = 0; i < nbRow; i++)
+        return target.x > -1 &&
+            target.x < 6 &&
+            target.y > -1 &&
+            target.y < 12;
+    }
+    public static bool FreeSpace(Vector3 target, Transform parentTransform)
+    {
+        if (WithinBorders(target))
         {
-            grid.Add(new List<Puya>());
-            for (int j = 0; j < nbColumn; j++)
+            return gameBoard[(int)target.x, (int)target.y] == null ||
+                gameBoard[(int)target.x, (int)target.y].parent == parentTransform;
+        }
+        return false;
+    }
+
+    public static bool IsEmpty(int col, int row)
+    {
+        if (WithinBorders(new Vector3(col, row, 0)))
+        {
+            return gameBoard[col, row] == null;
+        }
+        return false;
+    }
+
+    public static bool ColorMatches(int col, int row, Transform puyoUnit)
+    {
+        if (WithinBorders(new Vector3(col, row, 0)))
+        {
+            return gameBoard[col, row].GetComponent<PuyaUnit>().colorIdx == puyoUnit.GetComponent<PuyaUnit>().colorIdx;
+        }
+        return false;
+    }
+
+    public static bool HasMatchingNeighbor(Vector2 pos, Vector3 direction, Transform puyoUnitTransform)
+    {
+        Vector2 newPos = new Vector2(pos.x + direction.x, pos.y + direction.y);
+        return !IsEmpty((int)newPos.x, (int)newPos.y) && ColorMatches((int)newPos.x, (int)newPos.y, puyoUnitTransform);
+    }
+
+    public static void Clear(float col, float row)
+    {
+        gameBoard[(int)col, (int)row] = null;
+    }
+
+    public static void Add(float col, float row, Transform obj)
+    {
+        gameBoard[(int)col, (int)row] = obj;
+    }
+
+    public static void Delete(Transform puyo)
+    {
+        Vector2 pos = new Vector2(Mathf.Round(puyo.position.x), Mathf.Round(puyo.position.y));
+        gameBoard[(int)pos.x, (int)pos.y] = null;
+        UnityEngine.Object.Destroy(puyo.gameObject);
+    }
+
+    public static bool WhatToDelete()
+    {
+        List<Transform> groupToDelete = new List<Transform>();
+
+        for (int row = 0; row < 12; row++)
+        {
+            for (int col = 0; col < 6; col++)
             {
-                grid[i].Add(null);
+                List<Transform> currentGroup = new List<Transform>();
+
+                if (gameBoard[col, row] != null)
+                {
+                    Transform current = gameBoard[col, row];
+                    if (groupToDelete.IndexOf(current) == -1)
+                    {
+                        AddNeighbors(current, currentGroup);
+                    }
+                }
+
+                if (currentGroup.Count >= 4)
+                {
+                    foreach (Transform puyo in currentGroup)
+                    {
+                        groupToDelete.Add(puyo);
+                    }
+                }
             }
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public bool inBound(int x, int y)
-    {
-        if (x >= 0 && x < nbRow && y >= 0 && y < nbColumn)
+        if (groupToDelete.Count != 0)
         {
+            DeleteUnits(groupToDelete);
             return true;
         }
-        else return false;
-    }
-
-    public bool isFree(int x,int y)
-    {
-        if (grid[x][y] == null)
+        else
         {
-            return true;
+            return false;
         }
-        else return false;
     }
 
-    public void pushDownGrid()
+    public static void DropAllColumns()
     {
-        for(int i = nbRow - 2; i >= 0; i--)
+        for (int row = 0; row < 12; row++)
         {
-            for (int j =0; j < nbColumn-1; j++)
+            for (int col = 0; col < 6; col++)
             {
-                if (isFree(i + 1, j))
+                if (gameBoard[col, row] != null)
                 {
-                    grid[i + 1][j] = grid[i][j];
-                    grid[i][j] = null;
+                    Transform puyoUnit = gameBoard[col, row];
+                    puyoUnit.gameObject.GetComponent<PuyaUnit>().DropToFloorExternal();
                 }
             }
         }
     }
 
-    public void debugGrid()
+    static void AddNeighbors(Transform currentUnit, List<Transform> currentGroup)
     {
-        for (int i = 0; i < nbRow; i++)
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.right, Vector3.left };
+        if (currentGroup.IndexOf(currentUnit) == -1)
         {
-            string res = "";
-            for (int j = 0; j < nbColumn; j++)
+            currentGroup.Add(currentUnit);
+        }
+        else
+        {
+            return;
+        }
+
+        foreach (Vector3 direction in directions)
+        {
+            int nextX = (int)(Mathf.Round(currentUnit.position.x) + Mathf.Round(direction.x));
+            int nextY = (int)(Mathf.Round(currentUnit.position.y) + Mathf.Round(direction.y));
+
+            if (!IsEmpty(nextX, nextY) && ColorMatches(nextX, nextY, currentUnit))
             {
-                try
+                Transform nextUnit = gameBoard[nextX, nextY];
+                AddNeighbors(nextUnit, currentGroup);
+            }
+        }
+    }
+
+    static void DeleteUnits(List<Transform> unitsToDelete)
+    {
+        foreach (Transform unit in unitsToDelete)
+        {
+            Delete(unit);
+        }
+    }
+
+    public static bool AnyFallingBlocks()
+    {
+        for (int row = 11; row >= 0; row--)
+        {
+            for (int col = 0; col < 6; col++)
+            {
+                if (gameBoard[col, row] != null)
                 {
-                    res += grid[i][j].color.ToString() + " ";
-                }
-                catch(Exception e)
-                {
-                    res += 0 + " ";
+                    if (gameBoard[col, row].gameObject.GetComponent<PuyaUnit>().forcedDownwards)
+                    {
+                        return true;
+                    }
+                    else if (gameBoard[col, row].gameObject.GetComponent<PuyaUnit>().activelyFalling)
+                    {
+                        return true;
+                    }
                 }
             }
-            Debug.Log(res);
         }
+
+        return false;
+    }
+
+    public static void DebugBoard()
+    {
+        Text text = GameObject.Find("Text").GetComponent<Text>();
+        string boardContents = "";
+
+        for (int row = 11; row >= 0; row--)
+        {
+            boardContents += $"{row} :";
+            for (int col = 0; col < 6; col++)
+            {
+                if (gameBoard[col, row] == null)
+                {
+                    boardContents += "o ";
+                }
+                else
+                {
+                    int idx = gameBoard[col, row].gameObject.GetComponent<PuyaUnit>().colorIdx;
+                    string[] colorArray = { "B", "G", "R", "C" };
+                    boardContents += $"{colorArray[idx]} ";
+                }
+            }
+            boardContents += "\n";
+        }
+        text.text = boardContents;
     }
 }
